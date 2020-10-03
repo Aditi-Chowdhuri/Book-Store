308ad5e2bdc49be255bd4febb9f5e4e8afa72759
Aditi-Chowdhuri <aditinirapada.2018@vitstudent.ac.in>
2020-10-03 16:19:23 +0530

    first commit

---
+++ react-shopping-cart/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">Book Mania</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        Made By - Aditi Chowdhuri &#128151;
+      </footer>
+    </div>
+  );
+}
+
+export default App;
